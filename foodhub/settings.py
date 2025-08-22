32c9aa1507f06b5d9d4cc7f1f0c7a7ed961a9891
Sara <sara.rosati@tiscali.it>
2025-08-22 18:06:28 +0200

    Prepare initial Heroku deploy

--- foodhub/settings.py
+++ foodhub/settings.py
@@ -18,8 +18,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-insecure-key-change-me")
-DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+DEBUG = os.getenv("DEBUG", "True") == "False"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1",'.herokuapp.com').split(",") 
 
 # Application definition
 
