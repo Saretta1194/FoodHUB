d70709789781030c3ee2862d5040c1b0517c634b
Sara <sara.rosati@tiscali.it>
2025-08-24 16:04:13 +0200

    Add restaurants admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Restaurant
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "is_active", "opening_hours", "created_at")
+    list_filter = ("is_active", "created_at")
+    search_fields = ("name", "address", "owner__username", "owner__email")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",)
